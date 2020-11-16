72ee7eeddbde0ebfe51ef44dc8346e8383ebb829
Gabyblackroses <mgabypaz@hotmail.com>
2020-11-15 22:06:12 -0300

    sec

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAIYsEN1Iplwy9Dxx7Ff5iLHyeoC74CCFA",
+    authDomain: "prueba1-37063.firebaseapp.com",
+    databaseURL: "https://prueba1-37063.firebaseio.com",
+    projectId: "prueba1-37063",
+    storageBucket: "prueba1-37063.appspot.com",
+    messagingSenderId: "776586890122",
+    appId: "1:776586890122:web:15ca9e9ffd6e3d29461673",
+  };
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Toys from '../views/Toys.vue'
+import Login from '../views/Login.vue'
+import firebase from 'firebase';
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Toys',
+    component: Toys,
+    meta:{
+      login: true
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
@@ -24,6 +26,18 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  let user = firebase.auth().currentUser
+  let authRequired = to.matched.some(route => route.meta.login)
+  console.log(user)
+  console.log(authRequired)
+  if(!user && authRequired){
+    next('/login')
+  } else{
+    next()
+  }
 })
 
 export default router
